77835012ebe7d0deb09c2eb11c59cbe53fa07743
Johnny E. Delgado Rangel <jedelgado@bod.com.ve>
2020-12-09 16:54:25 -0400

    ya ordenado el form documento respecto a fechas y preguntas

--- resources/views/documento/formulario.blade.php
+++ resources/views/documento/formulario.blade.php
@@ -1,242 +1,9 @@
-<div class="row showName" style="margin: 10px;">
-            <div class="col-4 offset-4">
-                <input type="button" name="addfecha" id="addfecha" class="btn btn-success btn-block addmore" value="Agregar Fecha" />
-            </div>
-        </div>
 
- @if( old('fechaini') )
-@php
-//dd(old('fechaini'));
-@endphp
-    
-
-     @foreach( old('fechaini') as $i => $field)
-     <div id="dynamicFieldsXX">
- <!-- Start row -->
-                <div class="row removing{{$i}} mb-4">
-                    <div class="col-2">
-                        <div class="d-flex">
-                            <div class="p-1">
-                                <label class="showName p-2" for="date">Fechas</label>
-                            </div> 
-                        </div>
-                    </div>
-                    <div class="col-4"> 
-                        <div class="d-flex">                    
-                            <div class="flex-fill p-2">
-                                <input class="form-control showName" 
-                                        type="datetime-local" 
-                                        name="fechaini[{{$i}}]" value="{{old('fechaini.'.$i.'')}}"
-                                required>
-                            </div>
-                        </div> 
-                    </div>
-                    <div class="col-4"> 
-                        <div class="d-flex">                    
-                            <div class="flex-fill p-2">
-                                <input class="form-control showName" 
-                                        type="datetime-local" 
-                                        name="fechafin[{{$i}}]" value="{{old('fechafin.'.$i.'')}}"
-                                required>
-                            </div>
-                        </div> 
-                    </div>
-                    @isset($eseditar) 
-                    @if( old('usuario_id.'.$i.''))
-
-                    <div class="col-2"> 
-                        <div class="d-flex">                    
-                            <div class="flex-fill p-2">
-                                <input class="form-control showName" 
-                                        type="text" 
-                                        name="user_name[{{$i}}]" value="{{old('user_name.'.$i.'')}}"
-                                readonly required>
-                                <input class="form-control showName" 
-                                        type="hidden" 
-                                        name="usuario_id[{{$i}}]" value="{{old('usuario_id.'.$i.'')}}"
-                                >
-                            </div>
-                        </div> 
-                    </div>
-                    @endif
-                    @endisset
-                </div> <!-- End row --> 
-                            <div class="row removing{{$i}} showName" style="margin: 10px;">
-              <div class="col-4 offset-4">
-              
-               <input type="button" class="btn btn-danger btn-block remove-fields delfecha" id="removing{{$i}}" name="delfecha{{$i}}"  value="Remover Fecha">
-            </div>
-          </div>
-
-            </div> <!-- End dynamicFields -->
-
-       
- @php
-    if(empty($countarrayviejo)){
-        $countarrayviejo = 0;
-    }
-    if( $i > $countarrayviejo ){$countarrayviejo = $i;  }
-
-    //if($loop->last){$countarrayviejo = $i;  }
-    //$countarrayviejo = $loop->count;
-@endphp
-
-      @endforeach
- @php
-echo $countarrayviejo ;
-@endphp
-<input type="hidden" id="countarrayviejo" name="countarrayviejo" value="{{$countarrayviejo}}" />
-@endif
-
-
-
-<!-- Comienza empty es Editar fechas part1 -->
-@isset($eseditar) 
-@if(empty( old('fechaini') ))
-@php
-//echo "holandaDDDD";
-$i = 0;
-$dt = '2020-12-08T15:36';
-@endphp
-@foreach( $data->usuarios as $usuario)
-     <div id="dynamicFieldsXXY">
- <!-- Start row -->
-                <div class="row removing{{$i}} mb-4">
-                    <div class="col-2">
-                        <div class="d-flex">
-                            <div class="p-1">
-                                <label class="showName p-2" for="date">Fechas</label>
-                            </div> 
-                        </div>
-                    </div>
-                    <div class="col-4"> 
-                        <div class="d-flex">                    
-                            <div class="flex-fill p-2">
-                                <input class="form-control showName" 
-                                        type="datetime-local" 
-                                        name="fechaini[{{$i}}]" value="{{date('Y-m-d\TH:i', strtotime($usuario->pivot->fechaini))}}"
-                                required>
-                            </div>
-                        </div> 
-                    </div>
-                    <div class="col-4"> 
-                        <div class="d-flex">                    
-                            <div class="flex-fill p-2">
-                                <input class="form-control showName" 
-                                        type="datetime-local" 
-                                        name="fechafin[{{$i}}]" value="{{date('Y-m-d\TH:i', strtotime($usuario->pivot->fechafin))}}"
-                                required>
-                            </div>
-                        </div> 
-                    </div>
-                    <div class="col-2"> 
-                        <div class="d-flex">                    
-                            <div class="flex-fill p-2">
-                                <input class="form-control showName" 
-                                        type="text" 
-                                        name="user_name[{{$i}}]" value="{{$usuario->name}}"
-                                readonly required>
-                                <input class="form-control showName" 
-                                        type="hidden" 
-                                        name="usuario_id[{{$i}}]" value="{{$usuario->pivot->usuario_id}}"
-                                >
-                            </div>
-                        </div> 
-                    </div>
-                </div> <!-- End row --> 
-                            <div class="row removing{{$i}} showName" style="margin: 10px;">
-              <div class="col-4 offset-4">
-              
-               <input type="button" class="btn btn-danger btn-block remove-fields delfecha" id="removing{{$i}}" name="delfecha{{$i}}"  value="Remover Fecha">
-            </div>
-          </div>
-
-            </div> <!-- End dynamicFields -->
-
-       
- @php
-    if(empty($countarrayviejo)){
-
-        $countarrayviejo = 0;
-    }
-    if( $i > $countarrayviejo ){$countarrayviejo = $i;  }
-
-    //if($loop->last){$countarrayviejo = $i;  }
-    //$countarrayviejo = $loop->count;
-@endphp
-@php
-$i++;
-@endphp
-      @endforeach
- @php
-//echo $countarrayviejo ;
-@endphp
-<input type="hidden" id="countarrayviejo" name="countarrayviejo" value="{{$countarrayviejo ?? 0}}" />
-@endif   
-@endisset
-<!-- termina empty es Editar fechas part1-->
-<div id="dynamicFields">
-</div>
-
-
-
-            <div class="card-body table-responsive">
-               <div class="form-group "> 
-                <table class="table table-striped table-bordered table-hover" id="tabla-data">
-                    <thead id="thead">
-
-                    </thead>
-                    <tbody id="tbody">
-
-                    </tbody>
-                </table>
-                </div>
-            </div>
 
 <p id="ur"></p>
 <p id="r"></p>
 <div class="cc"></div>
-<!-- Comienza isset es Editar tabla-preguntas -->
-@isset($eseditar)    
-            <div class="card-body table-responsive">
-               <div class="form-group "> 
-                <table class="table table-striped table-bordered table-hover" id="tabla-data">
-                    <thead id="thead">
-                    <tr class="filaPregunta"><th>ID</th><th>Pregunta</th><th></th><th></th><th></th></tr>
-                    </thead>
-                    <tbody id="tbody">
 
-                                                  
-                                @foreach ($data->questions as $question)
-                                @php($i = $loop->iteration -1)
-                                <tr> 
-                                <td>
-                                    {{$loop->last ? $question->id : $question->id}}
-                                    <input class="form-control" id="question_id[]" name="question_id[]" type="hidden" value="{{$loop->last ? $question->id : $question->id}}">
-                                </td>
-                                <td>
-                                    {{$loop->last ? $question->name : $question->name}}
-                                </td>
-                                <td>
-                                    
-                                </td>
-                                <td>
-                                    {{$loop->last ? $question->pivot->state : $question->pivot->state}}
-                                </td>
-                                <td class="text-center" >
-                                    
-                                    <span class="checkbox"></span> <input class="stateClass bb{{$i}}" type="hidden" id="state[{{$i}}]" name="state[{{$i}}]" value="{{old('state.'.$i.'', $loop->last ? $question->pivot->state : $question->pivot->state)}}">
-                                </td>
-                                </tr>
-                                @endforeach
-                        
-                        
-                    </tbody>
-                </table>
-                </div>
-            </div>
-@endisset
-<!-- termina isset es Editar tabla-preguntas -->
 <!-- Inicia isset es Editar selector -->
 @isset($eseditar)    
 <div class="form-group ">
@@ -326,7 +93,225 @@ $i++;
 
 
 
+            <div class="card-body table-responsive">
+               <div class="form-group "> 
+                <table class="table table-striped table-bordered table-hover" id="tabla-data">
+                    <thead id="thead">
+
+                    </thead>
+                    <tbody id="tbody">
+
+                    </tbody>
+                </table>
+                </div>
+            </div>
+<!-- Comienza isset es Editar tabla-preguntas -->
+@isset($eseditar)    
+            <div class="card-body table-responsive">
+               <div class="form-group "> 
+                <table class="table table-striped table-bordered table-hover" id="tabla-data">
+                    <thead id="thead">
+                    <tr class="filaPregunta"><th>ID</th><th>Pregunta</th><th></th><th></th><th></th></tr>
+                    </thead>
+                    <tbody id="tbody">
+
+                                                  
+                                @foreach ($data->questions as $question)
+                                @php($i = $loop->iteration -1)
+                                <tr> 
+                                <td>
+                                    {{$loop->last ? $question->id : $question->id}}
+                                    <input class="form-control" id="question_id[]" name="question_id[]" type="hidden" value="{{$loop->last ? $question->id : $question->id}}">
+                                </td>
+                                <td>
+                                    {{$loop->last ? $question->name : $question->name}}
+                                </td>
+                                <td>
+                                    
+                                </td>
+                                <td>
+                                    {{$loop->last ? $question->pivot->state : $question->pivot->state}}
+                                </td>
+                                <td class="text-center" >
+                                    
+                                    <span class="checkbox"></span> <input class="stateClass bb{{$i}}" type="hidden" id="state[{{$i}}]" name="state[{{$i}}]" value="{{old('state.'.$i.'', $loop->last ? $question->pivot->state : $question->pivot->state)}}">
+                                </td>
+                                </tr>
+                                @endforeach
+                        
+                        
+                    </tbody>
+                </table>
+                </div>
+            </div>
+@endisset
+<!-- termina isset es Editar tabla-preguntas -->
 
 
+
+<div class="row showName" style="margin: 10px;">
+            <div class="col-4 offset-4">
+                <input type="button" name="addfecha" id="addfecha" class="btn btn-success btn-block addmore" value="Agregar Fecha" />
+            </div>
+        </div>
+
+
+ @if( old('fechaini') )
+
+    
+
+     @foreach( old('fechaini') as $i => $field)
+     <div id="dynamicFieldsXX">
+ <!-- Start row -->
+                <div class="row removing{{$i}} mb-4">
+                    <div class="col-2">
+                        <div class="d-flex">
+                            <div class="p-1">
+                                <label class="showName p-2" for="date">Fechas</label>
+                            </div> 
+                        </div>
+                    </div>
+                    <div class="col-4"> 
+                        <div class="d-flex">                    
+                            <div class="flex-fill p-2">
+                                <input class="form-control showName" 
+                                        type="datetime-local" 
+                                        name="fechaini[{{$i}}]" value="{{old('fechaini.'.$i.'')}}"
+                                required>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-4"> 
+                        <div class="d-flex">                    
+                            <div class="flex-fill p-2">
+                                <input class="form-control showName" 
+                                        type="datetime-local" 
+                                        name="fechafin[{{$i}}]" value="{{old('fechafin.'.$i.'')}}"
+                                required>
+                            </div>
+                        </div> 
+                    </div>
+                    @isset($eseditar) 
+                    @if( old('usuario_id.'.$i.''))
+
+                    <div class="col-2"> 
+                        <div class="d-flex">                    
+                            <div class="flex-fill p-2">
+                                <input class="form-control showName" 
+                                        type="text" 
+                                        name="user_name[{{$i}}]" value="{{old('user_name.'.$i.'')}}"
+                                readonly required>
+                                <input class="form-control showName" 
+                                        type="hidden" 
+                                        name="usuario_id[{{$i}}]" value="{{old('usuario_id.'.$i.'')}}"
+                                >
+                            </div>
+                        </div> 
+                    </div>
+                    @endif
+                    @endisset
+                </div> <!-- End row --> 
+                            <div class="row removing{{$i}} showName" style="margin: 10px;">
+              <div class="col-4 offset-4">
+              
+               <input type="button" class="btn btn-danger btn-block remove-fields delfecha" id="removing{{$i}}" name="delfecha{{$i}}"  value="Remover Fecha">
+            </div>
+          </div>
+
+            </div> <!-- End dynamicFields -->
+
+       
+
+        @if (empty($countarrayviejo))
+        <input type="hidden"  value="{{$countarrayviejo = 0}}" />
+        @endif  
+        @if( $i > $countarrayviejo )
+        <input type="hidden"  value="{{$countarrayviejo = $i}}" />
+        @endif
+
+      @endforeach
+
+<input type="hidden" id="countarrayviejo" name="countarrayviejo" value="{{$countarrayviejo ?? 0}}" />
+@endif
+
+<!-- Comienza empty es Editar fechas part1 -->
+@isset($eseditar) 
+@if(empty( old('fechaini') ))
+<input type="hidden"  value="{{$i = 0}}" />
+@foreach( $data->usuarios as $usuario)
+     <div id="dynamicFieldsXXY">
+ <!-- Start row -->
+                <div class="row removing{{$i}} mb-4">
+                    <div class="col-2">
+                        <div class="d-flex">
+                            <div class="p-1">
+                                <label class="showName p-2" for="date">Fechas</label>
+                            </div> 
+                        </div>
+                    </div>
+                    <div class="col-4"> 
+                        <div class="d-flex">                    
+                            <div class="flex-fill p-2">
+                                <input class="form-control showName" 
+                                        type="datetime-local" 
+                                        name="fechaini[{{$i}}]" value="{{date('Y-m-d\TH:i', strtotime($usuario->pivot->fechaini))}}"
+                                required>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-4"> 
+                        <div class="d-flex">                    
+                            <div class="flex-fill p-2">
+                                <input class="form-control showName" 
+                                        type="datetime-local" 
+                                        name="fechafin[{{$i}}]" value="{{date('Y-m-d\TH:i', strtotime($usuario->pivot->fechafin))}}"
+                                required>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-2"> 
+                        <div class="d-flex">                    
+                            <div class="flex-fill p-2">
+                                <input class="form-control showName" 
+                                        type="text" 
+                                        name="user_name[{{$i}}]" value="{{$usuario->name}}"
+                                readonly required>
+                                <input class="form-control showName" 
+                                        type="hidden" 
+                                        name="usuario_id[{{$i}}]" value="{{$usuario->pivot->usuario_id}}"
+                                >
+                            </div>
+                        </div> 
+                    </div>
+                </div> <!-- End row --> 
+                            <div class="row removing{{$i}} showName" style="margin: 10px;">
+              <div class="col-4 offset-4">
+              
+               <input type="button" class="btn btn-danger btn-block remove-fields delfecha" id="removing{{$i}}" name="delfecha{{$i}}"  value="Remover Fecha">
+            </div>
+          </div>
+
+            </div> <!-- End dynamicFields -->
+
+       
+@if (empty($countarrayviejo))
+        <input type="hidden"  value="{{$countarrayviejo = 0}}" />
+        @endif  
+        @if( $i > $countarrayviejo )
+        <input type="hidden"  value="{{$countarrayviejo = $i}}" />
+        @endif
+        <input type="hidden"  value="{{$i++}}" />
+      @endforeach
+
+<input type="hidden" id="countarrayviejo" name="countarrayviejo" value="{{$countarrayviejo ?? 0}}" />
+@endif   
+@endisset
+<!-- termina empty es Editar fechas part1-->
+
+
+
+
+        <div id="dynamicFields">
+</div>
 
 
