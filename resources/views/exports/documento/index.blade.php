d33980e4a9cb55aab8b13522dddaec8d599e5c36
Johnny E. Delgado Rangel <jedelgado@bod.com.ve>
2021-03-10 15:41:04 +0100

    ya finalizado el exporte en excel listar index documentos falta exporte excel individual en el ver

--- resources/views/exports/documento/index.blade.php
+++ resources/views/exports/documento/index.blade.php
@@ -1,16 +1,27 @@
 <table>
     <thead>
     <tr>
-        <th>tipo de ducumento</th>
+        <th>Tipo de Ducumento</th>
         <th>Identificador</th>
-        <th>Nro. control</th>
-        <th>titulo</th>
-        <th>tipo de solicitud</th>
-        <th>tipo de fecha</th>
+        <th>Nro. Control</th>
+        <th>Titulo</th>
+        <th>Tipo de Solicitud</th>
+        <th>Responsable Externo</th>
+        <th>Estatus</th>
+        <th>Observacion</th>
+        <th>Usuario en el rango de Fecha</th>
+        <th>Tipo de Registro</th>
+        <th>Fecha Inicial</th>
+        <th>Fecha Final</th>
+        <th>orden</th>
     </tr>
     </thead>
     <tbody>
     @foreach ($datas as $data)
+    @php($count = $loop->iteration)
+    @php($usuario = $data->usuarios)
+
+
     <tr>	
     		<td>
                                 @foreach ($data->tipodocs as $tipodoc)
@@ -26,21 +37,57 @@
 	          <td>{{ $tiposolicitud->name }}</td>
 	         @endif 
 		    @endforeach
-            
+            <td>
+                                @foreach ($data->refexternas as $refexterna)
+                                    {{$loop->last ? $refexterna->name : $refexterna->name . ', '}}
+                                @endforeach
+            </td>
+            <td>
+                                @foreach ($data->tipoestados as $tipoestado)
+                                    {{$loop->last ? $tipoestado->name : $tipoestado->name . ', '}}
+                                @endforeach
+            </td>
+            <td>{{ $data->observation }}</td>  
                                 @foreach ($data->tipofechas as $tipofecha)
                                 @php($i = $loop->iteration -1)
                                 	 @if( $tipofecha->name)
                                 	 @if( $i == 0)
+                                     <td>
+                                        {{$usuario[$i]->name}} {{$usuario[$i]->lastname}}
+                                     </td>
                                 	 <td>
 	                                    {{$tipofecha->name}}
 	                                 </td>
+                                     <td>
+                                        {{$usuario[$i]->pivot->fechaini}}
+                                     </td>
+                                     <td>
+                                        {{$usuario[$i]->pivot->fechafin}}
+                                     </td>
+                                     <td>
+                                        {{$count}}
+                                     </td>
+
 	</tr>
                                 	 @else
                                 	 <tr>
-                                	 	<td></td><td></td><td></td><td></td><td></td>
+                                	 	<td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+
+                                        <td>
+                                        {{$usuario[$i]->name}} {{$usuario[$i]->lastname}}
+                                        </td>
 	                                	<td>
 	                                    {{$tipofecha->name}}
 	                                    </td>
+                                        <td>
+                                        {{$usuario[$i]->pivot->fechaini}}
+                                        </td>
+                                        <td>
+                                        {{$usuario[$i]->pivot->fechafin}}
+                                        </td>
+                                        <td>
+                                        {{$count}}
+                                        </td>
 	                                 </tr>
 
 	                                 @endif 
